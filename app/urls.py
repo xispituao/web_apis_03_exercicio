22e7770b1c38324420d96f179e469e664772a975
Natanael <nataso.s11@gmail.com>
2019-11-17 23:03:42 -0300

    Modificando o import

--- app/urls.py
+++ app/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('profile-posts/', ProfilePostList.as_view(), name=ProfilePostList.name),
     path('profile-posts/<int:pk>/', ProfilePostDetail.as_view(), name=ProfilePostDetail.name),
     path('posts-comments/', PostCommentList.as_view(), name=PostCommentList.name),
-    path('posts-comments/<int:pk>/', PostCommentDetail.as_view(), name=PostCommentDetail.name)
+    path('posts-comments/<int:pk>/', PostCommentDetail.as_view(), name=PostCommentDetail.name),
+    path('posts/<int:pk>/comments/', CommentList.as_view(), name=CommentList.name),
+    path('posts/<int:post_pk>/comments/<int:comment_pk>/', CommentDetail.as_view(), name=CommentDetail.name)
 ] 
--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ from .models import *
 from .serializers import *
 from rest_framework import generics
 from rest_framework.reverse import reverse
+from django.http import Http404
 
 class ImportJson(APIView):
     def post(self, request, format=None):
@@ -13,20 +14,17 @@ class ImportJson(APIView):
         posts = request.data['posts']
         comments = request.data['comments']
 
-        for profile in profiles:
-            profile_serializer = ProfileSerializer(data=profile)
-            if profile_serializer.is_valid():
-                profile_serializer.save()
+        profile_serializer = ProfileSerializer(data=profiles, many=True)
+        if profile_serializer.is_valid():
+            profile_serializer.save()
 
-        for post in posts:
-            post_serializer = PostSerializer(data=post)
-            if post_serializer.is_valid():
-                post_serializer.save()
+        post_serializer = PostSerializer(data=posts, many=True)
+        if post_serializer.is_valid():
+            post_serializer.save()
 
-        for comment in comments:
-            comment_serializer = CommentSerializer(data=comment)
-            if comment_serializer.is_valid():
-                comment_serializer.save()
+        comment_serializer = CommentSerializer(data=comments, many=True)
+        if comment_serializer.is_valid():
+            comment_serializer.save()
 
 class ProfileList(generics.ListCreateAPIView):
     queryset = Profile.objects.all()
@@ -58,7 +56,48 @@ class PostCommentDetail(generics.ListAPIView):
     serializer_class = PostCommentSerializer
     name = 'post-comment-detail'
 
+class CommentList(generics.ListAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    name = 'comment-list'
+
+class CommentDetail(APIView):
+    name = 'comment-detail'
+
+    def get_comment(self, post_pk,comment_pk):
+        try:
+            post = Post.objects.get(pk=post_pk)
+            try:
+                comment =  post.comments.get(pk=comment_pk)
+                return comment
+            except Comment.DoesNotExist:
+                raise Http404
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, post_pk,comment_pk, format=None):
+        comment = self.get_comment(post_pk,comment_pk)
+        comment_s = CommentSerializer(comment)
+        return Response(comment_s.data)
+
+    def put(self, request, post_pk,comment_pk, format=None):
+        comment = self.get_comment(post_pk,comment_pk)
+        comment_data = request.data
+        comment_data['postId'] = post_pk
+        comment_s = CommentSerializer(comment, data=comment_data)
+        if comment_s.is_valid():
+            comment_s.save()
+            return Response(comment_s.data)
+        return Response(comment_s.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, post_pk,comment_pk, format=None):
+        comment = self.get_comment(post_pk,comment_pk)
+        comment.delete()
+        return Response(status=status.HTTP_200_OK)
+
 class ApiRoot(generics.GenericAPIView):
+    name = 'api-root'
+
     def get(self, request, *args, **kwargs):
         return Response({
             'profile': reverse(ProfileList.name, request=request),
