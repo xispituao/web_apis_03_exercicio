387c5846bcc9bc7fb5e62266b67af251462b23b0
Natanael <nataso.s11@gmail.com>
2019-11-17 22:01:17 -0300

    Adicionando algumas views

--- app/serializers.py
+++ app/serializers.py
@@ -11,12 +11,20 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = ['postId', 'name', 'email', 'body']
 
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profile
-        fields = ['name', 'email', 'address']
-
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
         fields = ['street', 'suite', 'city', 'zipcode']
+
+class ProfileSerializer(serializers.ModelSerializer):
+    address = AddressSerializer()
+
+    class Meta:
+        model = Profile
+        fields = ['name', 'email', 'address']
+
+    def create(self, validated_data):
+        address_data = validated_data.pop('address')
+        address = Address.objects.create(**address_data)
+        profile = Profile.objects.create(address=address, **validated_data)
+        return profile
--- app/views.py
+++ app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
 
-# Create your views here.
+class FileLoad(APIView):
+    def post(self, request, format=None):
+        profiles = request.data['users']
+        posts = request.data['posts']
+        comments = request.data['comments']
+
+        for profile in profiles:
+            profile_s = ProfileSerializer(data=profile)
+            if profile_s.is_valid():
+                profile_s.save()
+
+        for post in posts:
+            post_s = PostSerializer(data=post)
+            if post_s.is_valid():
+                post_s.save()
+
+        for comment in comments:
+            comment_s = CommentSerializer(data=comment)
+            if comment_s.is_valid():
+                comment_s.save()
